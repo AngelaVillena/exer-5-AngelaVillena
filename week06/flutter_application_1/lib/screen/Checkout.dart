6c0170c161e250fbccbc580de85ea6ebc970bd06
AngelaVillena <apvillena1@up.edu.ph>
2024-04-10 19:54:30 +0800

    first commit for exer 6

---
+++ week06/flutter_application_1/lib/screen/Checkout.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import '../model/Item.dart';
+import "package:provider/provider.dart";
+import '../provider/shoppingcart_provider.dart';
+
+class Checkout extends StatelessWidget {
+  const Checkout({super.key});
+  @override
+  Widget build(BuildContext context) {
+    bool a = true;
+    return Scaffold(
+      appBar: AppBar(title: const Text("Checkout")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          getItems(context),
+          computeCost(),
+          const Divider(height: 4, color: Colors.black),
+          Flexible(
+              child: Center(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                if (a)
+                  ElevatedButton(
+                      onPressed: () {
+                        context.read<ShoppingCart>().removeAll();
+
+                        a = false;
+                      },
+                      child: const Text("Pay Now!")),
+              ]))),
+          TextButton(
+            child: const Text("Go back to Product Catalog"),
+            onPressed: () {
+              Navigator.pushNamed(context, "/products");
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget getItems(BuildContext context) {
+    List<Item> products = context.watch<ShoppingCart>().cart;
+
+    return products.isEmpty
+        ? const Text('No Items yet!')
+        : Expanded(
+            child: Column(
+            children: [
+              Flexible(
+                  child: ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: const Icon(Icons.food_bank),
+                    title: Text(products[index].name),
+                  );
+                },
+              )),
+            ],
+          ));
+  }
+
+  Widget computeCost() {
+    return Consumer<ShoppingCart>(builder: (context, cart, child) {
+      return Text("Total cost to pay: ${cart.cartTotal}");
+    });
+  }
+}
